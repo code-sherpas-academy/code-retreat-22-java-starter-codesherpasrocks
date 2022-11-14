9e3890aa3ca4b9b0e650a966f56c19e4fd2cea6a
Esperanza <esperanza.aviles@clarity.ai>
2022-11-14 11:45:11 +0100

    refactor: simplified marsRoverExecuteForwardCommand test

--- app/src/test/java/gradle/java/latest/AcceptanceTest.java
+++ app/src/test/java/gradle/java/latest/AcceptanceTest.java
@@ -8,14 +8,15 @@ import org.junit.jupiter.api.Test;
 public class AcceptanceTest {
 
   @Test
-  void marsRoverExecuteCommands() {
-    String expectedResponse = "3,4,NORTH";
+  void marsRoverExecuteForwardCommand() {
+    // Initial position for the rover is 0,0,NORTH
+    String expectedResponse = "0,1,NORTH";
     MarsRover marsRover = new MarsRover();
     CommandInterpreter commandInterpreter = new CommandInterpreter();
     OutputFormatter outputFormatter = new OutputFormatter();
     CommandExecutor commandExecutor = new CommandExecutor(outputFormatter, commandInterpreter, marsRover);
 
-    String response = commandExecutor.execute("FLFL");
+    String response = commandExecutor.execute("F");
 
     assertThat(response).isEqualTo(expectedResponse);
   }
